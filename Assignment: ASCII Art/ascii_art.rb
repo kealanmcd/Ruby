cb5f3d4574200491d7bb8625984ed88f0894c8b5
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-26 18:54:30 +0100

    part 3

--- Assignment: ASCII Art/ascii_art.rb
+++ Assignment: ASCII Art/ascii_art.rb
@@ -1,5 +1,5 @@
 # Part 1 - Write a Ruby program that prompts a person to enter a number, and then outputs a triangle of asterisks of that height and length.
-
+=begin
 print "Enter a number: "
 input = gets.to_i
 i = 0
@@ -39,6 +39,34 @@ while i < input
   # print asterisks
   while k < i
     output += "*"
+    k += 1
+  end
+
+  puts output
+end
+=end
+# Optional: Part 3 - Update your program so that it draws a triangle of asterisks that is centered.
+
+print "Enter a number: "
+input = gets.to_i
+i = 0
+
+while i < input
+  i += 1
+  output = " "
+
+  j = 0
+  k = 0
+
+  # print spaces
+  while j < input - i
+    output += " "
+    j += 1
+  end
+
+  # print asterisks
+  while k < i
+    output += "* "
     k += 1
   end
 
