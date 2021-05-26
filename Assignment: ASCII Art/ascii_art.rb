8c8b484ceb38bcd69fe7f25afe6ce86652c115c4
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-26 18:45:55 +0100

    part 2

--- Assignment: ASCII Art/ascii_art.rb
+++ Assignment: ASCII Art/ascii_art.rb
@@ -16,3 +16,31 @@ while i < input
 
   puts output
 end
+
+# Part 2 - Update your program so that it outputs a triangle of asterisks that is aligned on the right side.
+
+print "Enter a number: "
+input = gets.to_i
+i = 0
+
+while i < input
+  i += 1
+  output = ""
+
+  j = 0
+  k = 0
+  
+  # print spaces
+  while j < input - i
+    output += " "
+    j += 1
+  end
+
+  # print asterisks
+  while k < i
+    output += "*"
+    k += 1
+  end
+
+  puts output
+end
