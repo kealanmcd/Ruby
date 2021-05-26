93c292bc318db3583f772e67840fbc40eb3a81d9
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-26 23:40:23 +0100

    DRY rewrite

--- Arrays/Code Along: Prices/array-prices.rb
+++ Arrays/Code Along: Prices/array-prices.rb
@@ -1,3 +1,5 @@
+#  Old Code
+=begin
 def total(prices)
   amount = 0
   index = 0
@@ -25,4 +27,37 @@ def show_discounts(prices)
     puts format("Your discount: $%.3f", amount_off)
     index += 1
   end
+end
+=end
+
+# New DRY code
+def do_something_with_every_item(array, operation)
+  if operation == "total" or operation == "refund"
+    amount = 0
+  end
+
+  index = 0
+
+  while index < array.length
+
+    if operation == "total"
+      amount += array[index]
+
+    elsif operation == "refund"
+      amount -= array[index]
+
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your discount: $%.2f", amount_off)
+
+    end
+
+    index += 1
+
+  end
+
+  if operation == "total" or operation == "refund"
+    return amount
+  end
+  
 end
