50e03f75a6b4f6e0fc195b81b37c21b569561824
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-28 00:33:31 +0100

    finding array elements we want, with a block

--- Arrays/Code-Along-Reviews/reviews.rb
+++ Arrays/Code-Along-Reviews/reviews.rb
@@ -1,11 +1,15 @@
 lines = []
 
-review_file = File.open("reviews.txt") do |review_file|
+File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-puts review_file.class 
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
+relevant_lines = []
 
-puts lines.length
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+
+  puts relevant_lines
