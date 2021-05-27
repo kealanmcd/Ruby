c311ddd9a75830f5b6d0adfe81052ed5b64ca1db
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-27 16:32:33 +0100

    put rock paper scissors game into method

--- Assignment: Rock Paper Scissors/rock-paper-scissors.rb
+++ Assignment: Rock Paper Scissors/rock-paper-scissors.rb
@@ -1,53 +1,57 @@
-accepted_moves = {
-  :rock => "scissors",
-  :paper => "rock",
-  :scissors => "paper"
-}
+def play_rock_paper_scissors
+  accepted_moves = {
+    :rock => "scissors",
+    :paper => "rock",
+    :scissors => "paper"
+  }
 
-winners_text = {
-  :rock => "Rock crushes scissors",
-  :paper => "Paper covers rock",
-  :scissors => "Scissors cuts paper"
-}
+  winners_text = {
+    :rock => "Rock crushes scissors",
+    :paper => "Paper covers rock",
+    :scissors => "Scissors cuts paper"
+  }
 
-player_one_wins = 0
-player_two_wins = 0
+  player_one_wins = 0
+  player_two_wins = 0
 
-puts "Rock Paper Scissors"
+  puts "Rock Paper Scissors"
 
-while player_one_wins < 2 && player_two_wins < 2
-  print "Player 1 - Enter your selection: "
-  player_one = gets.chomp.downcase
-  player_one_symbol = player_one.to_sym
+  while player_one_wins < 2 && player_two_wins < 2
+    print "Player 1 - Enter your selection: "
+    player_one = gets.chomp.downcase
+    player_one_symbol = player_one.to_sym
 
-  print "Player 2 - Enter your selection: "
-  player_two = gets.chomp.downcase
-  player_two_symbol = player_two.to_sym
+    print "Player 2 - Enter your selection: "
+    player_two = gets.chomp.downcase
+    player_two_symbol = player_two.to_sym
 
-  if (accepted_moves[player_one_symbol] && accepted_moves[player_two_symbol])
-    
-    if accepted_moves[player_one_symbol] == player_two
+    if (accepted_moves[player_one_symbol] && accepted_moves[player_two_symbol])
       
-      puts winners_text[player_one_symbol]
-      player_one_wins += 1
+      if accepted_moves[player_one_symbol] == player_two
+        
+        puts winners_text[player_one_symbol]
+        player_one_wins += 1
 
-    elsif accepted_moves[player_two_symbol] == player_one
+      elsif accepted_moves[player_two_symbol] == player_one
 
-      puts winners_text[player_two_symbol]
-      player_two_wins += 1
+        puts winners_text[player_two_symbol]
+        player_two_wins += 1
 
-    else 
-      puts "Tie!"
+      else 
+        puts "Tie!"
+      end
+
+    else
+      puts "No cheaters! Only Rock, Paper or Scissors are allowed."
     end
 
-  else
-    puts "No cheaters! Only Rock, Paper or Scissors are allowed."
   end
 
+  if player_one_wins == 2
+    puts "Player 1 wins"
+  else
+    puts "Player 2 wins"
+  end
 end
 
-if player_one_wins == 2
-  puts "Player 1 wins"
-else
-  puts "Player 2 wins"
-end
+play_rock_paper_scissors
