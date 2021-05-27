f52fe82c38edfe33226c1ccc15e9965b17cea264
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-27 23:50:49 +0100

    show_discounts updated to use .each

--- Arrays/Code Along: Prices/array-prices.rb
+++ Arrays/Code Along: Prices/array-prices.rb
@@ -15,10 +15,14 @@ def refund(prices)
 end
 
 def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
-    puts format("Your discount: $%.3f", amount_off)
-    index += 1
+  prices.each do |price|
+    amount_off = price / 3.0
+    puts format("Your discount: $%.2f", amount_off)
   end
 end
+
+prices = [3.99, 25.00, 8.99]
+
+puts format("%.2f", total(prices))
+puts format("%.2f", refund(prices))
+show_discounts(prices)
