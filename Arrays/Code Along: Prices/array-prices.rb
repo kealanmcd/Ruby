88b138b540618233797f187dddf4f2fee26daf29
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-27 23:44:14 +0100

    DRYing up our code with 'each' and blocks

--- Arrays/Code Along: Prices/array-prices.rb
+++ Arrays/Code Along: Prices/array-prices.rb
@@ -1,11 +1,7 @@
-#  Old Code
-=begin
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount += prices[index]
-    index += 1
+  prices.each do |price|
+    amount += price
   end
   amount
 end
@@ -27,37 +23,4 @@ def show_discounts(prices)
     puts format("Your discount: $%.3f", amount_off)
     index += 1
   end
-end
-=end
-
-# New DRY code
-def do_something_with_every_item(array, operation)
-  if operation == "total" or operation == "refund"
-    amount = 0
-  end
-
-  index = 0
-
-  while index < array.length
-
-    if operation == "total"
-      amount += array[index]
-
-    elsif operation == "refund"
-      amount -= array[index]
-
-    elsif operation == "show discounts"
-      amount_off = array[index] / 3.0
-      puts format("Your discount: $%.2f", amount_off)
-
-    end
-
-    index += 1
-
-  end
-
-  if operation == "total" or operation == "refund"
-    return amount
-  end
-  
 end
