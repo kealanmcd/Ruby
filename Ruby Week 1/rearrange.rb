7c26f40cb314a43d99046e9e298945c71759af1d
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-27 14:51:06 +0100

    rearrange clean up

--- Ruby Week 1/rearrange.rb
+++ Ruby Week 1/rearrange.rb
@@ -1,12 +1,14 @@
-number = "This will be assigned a number"
+number = ""
+number_converted  = number.to_i.to_s
 
-while number != number.to_i.to_s
+while number != number_converted
   print "Enter a number: "
   number = gets.chomp
+  number_converted = number.to_i.to_s
 end
 
 is_positive_number = number[0] != '-' ? true : false
-largest_number =  number.split('')
+largest_number = number.split('')
 
 if is_positive_number
   largest_number = largest_number.sort { |a, b| b <=> a }.join
