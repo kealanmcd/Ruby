34afede41ee167131aee47e0f2e1765ca4b36c68
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-28 01:00:14 +0100

    find adjectives in each review

--- Arrays/Code-Along-Reviews/reviews.rb
+++ Arrays/Code-Along-Reviews/reviews.rb
@@ -7,5 +7,21 @@ end
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 puts relevant_lines
 
+
 reviews = relevant_lines.reject { |line| line.include?("--") }
 puts reviews
+
+
+adjectives = []
+
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+reviews.each do |review|
+  adjectives << find_adjective(review)
+end
+
+puts adjectives
