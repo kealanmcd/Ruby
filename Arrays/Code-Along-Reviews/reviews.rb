1fe85eb50cd9e4830516821342c0873b6b4f2107
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-05-28 01:06:44 +0100

    finished product

--- Arrays/Code-Along-Reviews/reviews.rb
+++ Arrays/Code-Along-Reviews/reviews.rb
@@ -1,3 +1,9 @@
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
 lines = []
 
 File.open("reviews.txt") do |review_file|
@@ -5,23 +11,12 @@ File.open("reviews.txt") do |review_file|
 end
 
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
-puts relevant_lines
-
-
 reviews = relevant_lines.reject { |line| line.include?("--") }
-puts reviews
 
-
-adjectives = []
-
-def find_adjective(string)
-  words = string.split(" ")
-  index = words.find_index("is")
-  words[index + 1]
+adjectives = reviews.map do |review|
+  adjective = find_adjective(review)
+  "'#{adjective.capitalize}'"
 end
 
-reviews.each do |review|
-  adjectives << find_adjective(review)
-end
-
+puts "the critics agree, Truncated is:"
 puts adjectives
