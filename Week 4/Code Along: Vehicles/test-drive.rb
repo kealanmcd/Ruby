98b02babb2931f15315810567fe5be25c41e457e
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-01 23:55:09 +0100

    test-drive part 2

--- Week 4/Code Along: Vehicles/test-drive.rb
+++ Week 4/Code Along: Vehicles/test-drive.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
 
   attr_accessor :odometer
   attr_accessor :gas_used
@@ -18,48 +18,5 @@ class Car
   def steer
     puts "Turn front 2 wheels."
   end
-end
 
-class Truck
-
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate
-    puts "Floor it!"
-  end
-  
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
-end
-
-class Motorcycle
-
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate
-    puts "Floor it!"
-  end
-  
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
 end
