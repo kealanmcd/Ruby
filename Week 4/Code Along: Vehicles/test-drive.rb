3d2b3f9d092eb613970bff4681a6cd8468cb2ce2
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-02 00:06:54 +0100

    test-drive part 4

--- Week 4/Code Along: Vehicles/test-drive.rb
+++ Week 4/Code Along: Vehicles/test-drive.rb
@@ -32,6 +32,12 @@ class Truck < Vehicle
 
 end
 
+class Car < Vehicle
+end
+
+class Motorcycle < Vehicle
+end
+
 truck = Truck.new
 truck.load_bed("259 bouncy balls")
 puts "The truck is carrying #{truck.cargo}"
@@ -41,3 +47,42 @@ truck.gas_used = 366
 
 puts "Average MPG:"
 puts truck.mileage
+
+car = Car.new
+puts car.instance_variables
+
+car.odometer = 22914
+car.gas_used = 728
+puts car.instance_variables
+
+
+
+
+class Person
+  
+  def name=(new_value)
+    @storage = new_value
+  end
+
+  def name
+    @storage
+  end
+
+end
+
+class Employee < Person
+
+  def salary=(new_value)
+    @storage = new_value
+  end
+
+  def salary
+    @storage
+  end
+
+end
+
+employee = Employee.new
+employee.name = "John Smith"
+employee.salary = 80000
+puts employee.name
