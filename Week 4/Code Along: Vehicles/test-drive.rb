93b250449fb9ee47d152200ee317ca657f90ca63
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-01 23:59:33 +0100

    test-drive part 3

--- Week 4/Code Along: Vehicles/test-drive.rb
+++ Week 4/Code Along: Vehicles/test-drive.rb
@@ -20,3 +20,24 @@ class Vehicle
   end
 
 end
+
+class Truck < Vehicle
+  
+  attr_accessor :cargo
+
+  def load_bed(contents)
+    puts "Securing #{contents} in the truck bed."
+    @cargo = contents
+  end
+
+end
+
+truck = Truck.new
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}"
+
+truck.odometer = 11432
+truck.gas_used = 366
+
+puts "Average MPG:"
+puts truck.mileage
