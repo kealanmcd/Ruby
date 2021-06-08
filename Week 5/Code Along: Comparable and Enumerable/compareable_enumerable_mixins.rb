418c2bb25f6bd8f37b6d29e343e9ee5d91bd1121
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-08 20:58:55 +0100

    comparable and enumberable part 4

--- Week 5/Code Along: Comparable and Enumerable/compareable_enumerable_mixins.rb
+++ Week 5/Code Along: Comparable and Enumerable/compareable_enumerable_mixins.rb
@@ -1,5 +1,7 @@
 class Steak
 
+  include Comparable
+
   GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
 
   attr_accessor :grade
@@ -31,3 +33,45 @@ end
 
 puts first_steak <=> second_steak
 puts second_steak <=> first_steak
+
+
+class WordSplitter
+
+  include Enumerable
+
+  attr_accessor :string
+
+  def each
+    string.split(" ").each do |word|
+      yield word
+    end
+  end
+
+end
+
+=begin
+splitter = WordSplitter.new
+splitter.string = "how do you do"
+
+p splitter.find_all { |word| word.include?("d") }
+p splitter.reject { |word| word.include?("d") }
+p splitter.map { |word| word.reverse }
+
+p splitter.any? { |word| word.include?("e") }
+p splitter.count
+p splitter.first
+p splitter.sort
+=end
+
+splitter = WordSplitter.new
+splitter.string = "salad beefcake corn beef pasta beefy"
+
+p splitter.find_all { |word| word.include?("beef") }
+p splitter.reject { |word| word.include?("beef") }
+p splitter.map { |word| word.capitalize }
+p splitter.count
+p splitter.find { |word| word.include?("beef") }
+p splitter.first
+p splitter.group_by { |word| word.include?("beef") }
+p splitter.max_by { |word| word.length }
+p splitter.to_a
