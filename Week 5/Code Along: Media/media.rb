328396e41ea9b353f36775d7eace766cf5710b04
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-08 19:34:13 +0100

    Code Along: Media module mixin final

--- Week 5/Code Along: Media/media.rb
+++ Week 5/Code Along: Media/media.rb
@@ -1,10 +1,6 @@
 module AcceptsComments
   def comments
-    if @comments
-      @comments
-    else
-      @comments = []
-    end
+    @comments ||= []
   end
 
   def add_comment(comment)
@@ -28,13 +24,20 @@ class Song < Clip
   attr_accessor :beats_per_minute
 end
 
+class Photo
+  include AcceptsComments
+  def initialize
+    @format = 'JPEG'
+  end
+end
+
+
 video = Video.new
 video.add_comment("Cool slow motion effect!")
 video.add_comment("Weird ending.")
 song = Song.new
 song.add_comment("Awesome beat.")
+photo = Photo.new
+photo.add_comment("Beautfiul colors.")
 
-p video.comments, song.comments
-
-video.play
-song.play
+p video.comments, song.comments, photo.comments
