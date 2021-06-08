826db1a3502c2a59b40065b4dc30dfb067f61946
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-08 19:12:24 +0100

    Code Along: Media module mixin

--- Week 5/Code Along: Media/media.rb
+++ Week 5/Code Along: Media/media.rb
@@ -1,23 +1,30 @@
-class Clip
-  attr_reader :comments
-  def initialize
-    @comments = []
+module AcceptsComments
+  def comments
+    if @comments
+      @comments
+    else
+      @comments = []
+    end
   end
 
   def add_comment(comment)
     comments << comment
   end
+end
 
+class Clip
   def play
     puts "Playing #{object_id}..."
   end
 end
 
 class Video < Clip
+  include AcceptsComments
   attr_accessor :resolution
 end
 
 class Song < Clip
+  include AcceptsComments
   attr_accessor :beats_per_minute
 end
 
@@ -28,3 +35,6 @@ song = Song.new
 song.add_comment("Awesome beat.")
 
 p video.comments, song.comments
+
+video.play
+song.play
