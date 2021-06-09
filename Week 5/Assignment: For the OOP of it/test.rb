d5393f671ae84bec75790111768ad522e7aa5f0c
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-09 21:52:32 +0100

    oop of it extra tests

--- Week 5/Assignment: For the OOP of it/test.rb
+++ Week 5/Assignment: For the OOP of it/test.rb
@@ -7,9 +7,18 @@ car = Car.new("Ford", 4, 4, "saloon")
 car.drive
 car.sound_horn
 car.to_s
+car.insert_passenger("Tom")
+car.insert_passenger("John")
+car.insert_passenger("Wesley")
+car.insert_passenger("Mary")
+car.insert_passenger("Frank")
+
 
 bus = Bus.new("Mercedez", 6, 40, 2000)
 bus.to_s
+bus.insert_passenger("Frank")
+
 
 plane = Plane.new("Airbus A380", 12, 800, "jet")
 plane.to_s
+plane.insert_passenger("Roger")
