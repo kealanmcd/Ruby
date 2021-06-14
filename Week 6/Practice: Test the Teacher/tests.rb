380576a2a4444a3ad4462b5797095d842b2caf0b
Kealan McDevitt <kealan.mcdevitt@shopify.com>
2021-06-14 10:12:01 +0100

    teacher tests update

--- Week 6/Practice: Test the Teacher/tests.rb
+++ Week 6/Practice: Test the Teacher/tests.rb
@@ -6,17 +6,17 @@ class MyTests < Minitest::Test
   input = File.read("day04-input.txt").strip
   passphrases = input.split("\n")
 
-  passphrases.each do |phrase|
+  passphrases.each_with_index do |phrase, index|
     words = phrase.split(" ")
     
-    define_method("test_phrase_has_no_repeating words") do
+    define_method("test_phrase_has_no_repeating_words_#{index}") do
       orignal_password = words
       duplicates_removed = words.uniq      
       assert_equal(orignal_password, duplicates_removed)
     end
 
 
-    define_method("test_phrase_has_no_words_that_are_anagrams_of_each_other") do
+    define_method("test_phrase_has_no_words_that_are_anagrams_of_each_other_#{index}") do
       original_password = words.map {|i| i.chars.sort.join }
       duplicates_removed = words.uniq
       assert_equal(original_password, duplicates_removed)
